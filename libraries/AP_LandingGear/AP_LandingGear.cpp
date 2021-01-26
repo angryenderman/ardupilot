af0ae7b95ecc6f3d03ab1158741856a3de348836
angryenderman <50720094+angryenderman@users.noreply.github.com>
2021-01-26 21:11:35 +0100

    Update AP_LandingGear.cpp

--- libraries/AP_LandingGear/AP_LandingGear.cpp
+++ libraries/AP_LandingGear/AP_LandingGear.cpp
@@ -148,7 +148,7 @@ void AP_LandingGear::retract()
 {
     // set servo PWM to retracted position
     SRV_Channels::set_output_limit(SRV_Channel::k_landing_gear_control, SRV_Channel::Limit::MIN);
-    SRV_Channels::set_output_scaled(SRV_Channel::k_scripting2, 4500);
+    SRV_Channels::set_output_pwm(SRV_Channel::k_scripting2, 0);
 
     // reset deployed flag
     _deployed = false;
